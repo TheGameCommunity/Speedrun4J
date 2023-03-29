80fe474f5ce3bb61497331a0d02aab1805f0dcb1
Gamebuster19901 <Gamebuster19901@users.noreply.github.com>
2023-03-29 16:02:41 -0400

    update ruleset

--- src/main/java/com/tsunderebug/speedrun4j/game/run/Ruleset.java
+++ src/main/java/com/tsunderebug/speedrun4j/game/run/Ruleset.java
@@ -2,12 +2,15 @@ package com.tsunderebug.speedrun4j.game.run;
 
 import com.google.gson.JsonObject;
 import com.tsunderebug.speedrun4j.JsonData;
+import com.tsunderebug.speedrun4j.Speedrun4J;
 
 public class Ruleset implements JsonData {
 	
+	private Speedrun4J s4j;
 	private JsonObject ruleData;
 	
-	public Ruleset(JsonObject ruleData) {
+	public Ruleset(Speedrun4J s4j, JsonObject ruleData) {
+		this.s4j = s4j;
 		this.ruleData = ruleData;
 	}
 	
@@ -19,6 +22,18 @@ public class Ruleset implements JsonData {
 		return ruleData.get("require-verification").getAsBoolean();
 	}
 	
+	/*
+	public TimingMethod[] getTimingMethods () {
+		
+	}
+	*/
+	
+	/*
+	public TimingMethod getDefaultTimingMethod() {
+		
+	}
+	*/
+	
 	public boolean requiresVideo() {
 		return ruleData.get("require-video").getAsBoolean();
 	}
@@ -27,8 +42,14 @@ public class Ruleset implements JsonData {
 		return ruleData.get("emulators-allowed").getAsBoolean();
 	}
 	
+	@Override
 	public JsonObject getData() {
 		return ruleData.deepCopy();
+	}
+
+	@Override
+	public Speedrun4J getS4j() {
+		return s4j;
 	}
 
 }
