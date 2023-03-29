5fdf5a755dae263cc5d33a9676501f4e7d77a420
Gamebuster19901 <Gamebuster19901@users.noreply.github.com>
2023-03-29 16:06:15 -0400

    Update moderator type

--- src/main/java/com/tsunderebug/speedrun4j/user/ModeratorType.java
+++ src/main/java/com/tsunderebug/speedrun4j/user/ModeratorType.java
@@ -2,8 +2,40 @@ package com.tsunderebug.speedrun4j.user;
 
 public enum ModeratorType {
 
-	MODERATOR,
-	SUPER_MODERATOR,
-	VERIFIER
+	UNKNOWN(Integer.MIN_VALUE, "unknown-moderation-rank"),
+	VERIFIER(-1, "verifier"),
+	MODERATOR(0, "moderator"),
+	SUPER_MODERATOR(1, "super-moderator");
+
+	private final int index;
+	private final String name;
+	
+	ModeratorType(int i, String name) {
+		this.index = i;
+		this.name = name;
+	}
+	
+	public static ModeratorType get(String name) {
+		for(ModeratorType modType : values()) {
+			if(modType.name.equals(name)) {
+				return modType;
+			}
+		}
+		return UNKNOWN;
+	}
+	
+	public static ModeratorType get(int index) {
+		switch(index) {
+			case -1:
+				return VERIFIER;
+			case 0:
+				return MODERATOR;
+			case 1:
+				return SUPER_MODERATOR;
+			default:
+				return UNKNOWN;
+		}
+	}
+	
 	
 }
